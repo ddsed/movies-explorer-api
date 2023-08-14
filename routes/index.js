a525bcfd37418baa582e8ca8068e8a52ff0e81c1
Dasha Sedova <dashassed@gmail.com>
2023-08-14 19:06:40 +0300

    feat: movie route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRouter = require('./users');
-// const cardRouter = require('./cards');
+const movieRouter = require('./movies');
 const { createUser, loginUser } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const { validationUserInfo } = require('../middlewares/validation');
@@ -12,7 +12,7 @@ router.post('/signin', validationUserInfo, loginUser);
 router.use(auth);
 
 router.use('/users', userRouter);
-// router.use('/cards', cardRouter);
+router.use('/movies', movieRouter);
 
 router.use((req, res, next) => {
   next(new NotFoundError('Такой страницы не существует'));
